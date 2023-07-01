c23db15a08f3db4c1cad36e1fea5540e501d7def
Rohan <rohanrc101@gmail.com>
2023-07-01 19:28:46 +0300

    feat: splash screen

---
+++ app/src/main/java/com/ph03nixc0ders/njilaka/views/Home.kt
@@ -0,0 +1,12 @@
+package com.ph03nixc0ders.njilaka.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ph03nixc0ders.njilaka.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
