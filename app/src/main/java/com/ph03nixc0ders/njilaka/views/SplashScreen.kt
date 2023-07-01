2c5e479f8117e49f49aada99cd0d606f108ace3a
Rohan <rohanrc101@gmail.com>
2023-07-01 18:44:44 +0300

    feat: creation of SplashScreen

---
+++ app/src/main/java/com/ph03nixc0ders/njilaka/views/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.ph03nixc0ders.njilaka.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ph03nixc0ders.njilaka.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
