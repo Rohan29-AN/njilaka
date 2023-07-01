326ff63f9e17cf84739f432861c55a358a347cda
Rohan <rohanrc101@gmail.com>
2023-07-01 22:47:29 +0300

    feat: implement random result generation

--- app/src/main/java/com/ph03nixc0ders/njilaka/views/Home.kt
+++ app/src/main/java/com/ph03nixc0ders/njilaka/views/Home.kt
@@ -15,6 +15,7 @@ import com.anupkumarpanwar.scratchview.ScratchView
 import com.ph03nixc0ders.njilaka.R
 import com.ph03nixc0ders.njilaka.databinding.ActivityHomeBinding
 import com.ph03nixc0ders.njilaka.databinding.ActivitySplashScreenBinding
+import com.ph03nixc0ders.njilaka.utils.Utilities
 
 class Home : AppCompatActivity() {
 
@@ -35,14 +36,40 @@ class Home : AppCompatActivity() {
         this.supportActionBar?.hide()
 
 
+        //generate results
+        this._binding.generate.setOnClickListener{
+            var number=this._binding.numberSeries.text.toString()
+            var from=this._binding.from.text.toString()
+            var to=this._binding.to.text.toString()
+
+            if(number.isEmpty() || from.isEmpty() || to.isEmpty()){
+                Toast.makeText(this,"Fenoy tsara daholo azafady",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                var result=Utilities.generateRandomNumber(number.toInt(),from.toInt(),to.toInt())
+
+                //Append the result on the textView
+                this._binding.resultat.text=result
+
+                //display the scratchView
+                this._binding.resultatLinear.visibility=View.VISIBLE
+            }
+        }
+
+
+        //reinit the scratchView
+        this._binding.restart.setOnClickListener{
+             recreate()
+        }
+
         this._binding.scratchView.setRevealListener(object :ScratchView.IRevealListener{
             override fun onRevealed(scratchView: ScratchView?) {
+                Toast.makeText(this@Home,"Wow, mahay mikiky be ianao",Toast.LENGTH_SHORT).show()
                 _binding.scratchView?.visibility=View.GONE
             }
 
             override fun onRevealPercentChangedListener(scratchView: ScratchView?, percent: Float) {
-                Toast.makeText(this@Home,"Wow, mahay mikiky be ianao",Toast.LENGTH_SHORT).show()
-            }
+                 }
         })
     }
 
